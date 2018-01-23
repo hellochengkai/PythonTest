20f17cb328111d0e52257522f646ccf22f846c5e
chengkai <hellochengkai@hotmail.com>
2018-01-23 22:48:39 +0800

    添加第一个py 函数

---
+++ 1/nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		=	"nester",
+	version		=	"1.0.0",
+	py_modules	=	['nester'],
+	author		=	'chengkai',
+	author_email=	'chengkai@thunder.com.cn',
+	url			=	't.tt',
+	description	=	'test setup',
+	)
